56a9c5c1a6c6e5c85d26e7c4c5b554dd25fc6ce9
Jeremy <jeremy.studzinski@gmail.com>
2018-04-04 02:41:40 -0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>EL Bracket Site</h1>
+    <button @click="getApi">Get api</button>
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+  const BASE_URL = 'http://localhost:5000'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    methods: {
+      getApi() {
+        this.$http.get(BASE_URL + '/api')
+          .then(response => {
+            console.log(response.body.msg)
+            this.msg = response.body.msg
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
